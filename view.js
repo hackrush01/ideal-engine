205003d4e88e281c65e2bd0c2db33515f930fa9e
utkarsh792 <shukla.utkarsh10@gmail.com>
2019-03-13 19:11:33 +0530

    Input through CSV file enabled.

--- view.js
+++ view.js
@@ -21,6 +21,58 @@ $('#add-to-list').on('click', () => {
 })
 
 
+
+$('#add-to-list-from-csv').on('click', () => {
+
+ var fileUpload = document.getElementById("fileUpload");
+
+    if (!fileUpload.value.endsWith(".csv") && !fileUpload.value.endsWith(".txt")){
+        alert("Please upload a valid CSV file.");
+    } else if (typeof(FileReader) == "undefined" && typeof(Storage) == "undefined"){
+        alert("This browser does not support HTML5.");
+    } else {
+        do_upload(fileUpload);
+    }
+})
+
+
+function do_upload(file){
+    var reader = new FileReader();
+    reader.readAsText(fileUpload.files[0]);
+    
+    reader.onload = (e) => {
+        var rows = e.target.result.split("\r\n");
+        var groups = {};
+
+        if (rows.length < 1) {
+            alert("Empty csv file");
+            return
+        }
+
+        for (var i = 0; i < rows.length; i++) {
+            if(rows[i] == "") continue
+         
+            const cells = rows[i].split(",");
+          if(cells[0] == "") continue
+            const group_number = cells[3];
+            const student_detail = {name: cells[0], gh_handle: cells[1], repository: cells[2]};
+
+            if (group_number in groups){
+                groups[group_number].push(student_detail)
+            } else {
+                groups[group_number] = [student_detail];
+            }
+        }
+
+        for (var key in groups) {
+            localStorage.setItem(key, JSON.stringify(groups[key]));
+        }
+    }
+    document.getElementById("fileUpload").value = "";
+}
+
+
+
 $('#del-from-list').on('click', () => {
 	// fs.unlink(filename, function(err)
 	// {
